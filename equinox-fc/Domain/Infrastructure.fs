0b1c2756e3eafeda66bd6d4df565753a34685611
Ruben Bartelink <ruben@bartelink.com>
2019-11-30 00:44:05 +0000

    Allocation example from Equinox 174

--- equinox-fc/Domain/Infrastructure.fs
+++ equinox-fc/Domain/Infrastructure.fs
@@ -2,6 +2,8 @@ namespace global
 
 open FSharp.UMX // see https://github.com/fsprojects/FSharp.UMX - % operator and ability to apply units of measure to Guids+strings
 
+(* Locations *)
+
 type LocationId = string<locationId>
 and [<Measure>] locationId
 module LocationId =
@@ -14,3 +16,29 @@ module LocationEpochId =
     let parse (value : int) : LocationEpochId = %value
     let next (value : LocationEpochId) : LocationEpochId = % (%value + 1)
     let toString (value : LocationEpochId) : string = string %value
+
+(* Tickets *)
+
+type TicketId = string<ticketId>
+and [<Measure>] ticketId
+module TicketId =
+    let parse (value : string) : TicketId = let raw = value in %raw
+    let toString (value : TicketId) : string = %value
+
+type TicketListId = string<ticketListId>
+and [<Measure>] ticketListId
+module TicketListId =
+    let parse (value : string) : TicketListId = let raw = value in %raw
+    let toString (value : TicketListId) : string = %value
+
+type AllocationId = string<allocationId>
+and [<Measure>] allocationId
+module AllocationId =
+    let parse (value : string) : AllocationId = let raw = value in %raw
+    let toString (value : AllocationId) : string = %value
+
+type AllocatorId = string<allocatorId>
+and [<Measure>] allocatorId
+module AllocatorId =
+    let parse (value : string) : AllocatorId = let raw = value in %raw
+    let toString (value : AllocatorId) : string = %value
